cc98c29ba39516531531a7b36ecedadb07eddbd6
Gustavo Czuhajowskyj <148733478+czuy1@users.noreply.github.com>
2024-12-11 13:00:05 -0300

    Add files via upload

---
+++ sitios web/funcion.js
@@ -0,0 +1,12 @@
+function dojo(){
+    location.href="inoue.html"
+};
+function torneos(){
+    location.href="torneos.html"
+};
+function federacion(){
+    location.href="https://www.karatefak.com.ar/"
+};
+function mundial(){
+    location.href="https://www.wkf.net/"
+};
